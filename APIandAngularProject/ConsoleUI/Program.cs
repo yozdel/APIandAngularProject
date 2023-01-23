05bc5ff2b30e8638b074e0b4a4727a32b2b82ff8
yozdel <ozdelyasin@gmail.com>
2023-01-23 22:06:37 +0300

    Created
    The project was created.
    Database connection established with Entity framework.

---
+++ APIandAngularProject/ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductManager ProductManager = new ProductManager(new EfProductDal());
+foreach (var product in ProductManager.GetAllByUnitPrice(40,100))
+{
+    Console.WriteLine(product.ProductName);
+}
+
+
