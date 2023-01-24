6373f6a918dc7141abecd97f2cb02688294e8583
yozdel <ozdelyasin@gmail.com>
2023-01-24 22:53:37 +0300

    Added core layer
    Added core layer
    
    Added dtos to the entities layer.

--- APIandAngularProject/ConsoleUI/Program.cs
+++ APIandAngularProject/ConsoleUI/Program.cs
@@ -3,11 +3,25 @@
 using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 
-ProductManager ProductManager = new ProductManager(new EfProductDal());
-foreach (var product in ProductManager.GetAllByUnitPrice(40,100))
+ProductTest();
+//CategoryTest();
+
+static void ProductTest()
 {
-    Console.WriteLine(product.ProductName);
+    ProductManager ProductManager = new ProductManager(new EfProductDal());
+    foreach (var product in ProductManager.GetProductDetails())
+    {
+        Console.WriteLine(product.ProductName + "/"+ product.CategoryName);
+    }
 }
 
-
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
+}
